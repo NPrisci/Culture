6ead4b2abbcf924e1a2c5736be733d532354e28a
NPrisci <priscilialauress@gmail.com>
2025-12-15 10:15:52 +0100

    Refactor RoleSeeder to manage foreign key checks
    Updated role seeder to disable foreign key checks and truncate roles table before seeding.

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,37 +4,29 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
+use Illuminate\Support\Facades\DB;
 
 class RoleSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
-        $this ->call([
-            RoleSeeder::class,
-        ]);
+        // Désactiver les vérifications de clés étrangères
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Role::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         $roles = [
-            [
-                'id_role' => 1,
-                'nom_role' => 'Administrateur',
-            ],
-            [
-                'id_role' => 2,
-                'nom_role' => 'Modérateur',
-            ],
-            [
-                'id_role' => 3,
-                'nom_role' => 'Utilisateur',
-            ],
-            
+            ['id_role' => 1, 'nom_role' => 'Administrateur'],
+            ['id_role' => 2, 'nom_role' => 'Utilisateur'],
+            ['id_role' => 3, 'nom_role' => 'Modérateur'],
         ];
+
         foreach ($roles as $role) {
             Role::create($role);
         }
-        
     }
 }
